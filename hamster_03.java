4c05f40aa53b231b3c52827175729321c0e2401c
Kerstin Kollmann <code@kerstinkollmann.com>
2013-08-20 13:11:33 +0200

    programmed 3rd hamster

--- hamster_03.java
+++ hamster_03.java
@@ -1,4 +1,4 @@
-/*imperative program*/void main() {
+import de.hamster.debugger.model.Territorium;import de.hamster.debugger.model.Territory;import de.hamster.model.HamsterException;import de.hamster.model.HamsterInitialisierungsException;import de.hamster.model.HamsterNichtInitialisiertException;import de.hamster.model.KachelLeerException;import de.hamster.model.MauerDaException;import de.hamster.model.MaulLeerException;import de.hamster.model.MouthEmptyException;import de.hamster.model.WallInFrontException;import de.hamster.model.TileEmptyException;public class hamster_03 extends de.hamster.debugger.model.IHamster implements de.hamster.model.HamsterProgram {public void main() {
 
 	/* make hamster climb stairs */
 	
@@ -133,4 +133,5 @@
     linksUm();
     linksUm();              
     
+}
 }
